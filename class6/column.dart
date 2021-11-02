2893b56ba2a054c3fc790b24ded888a5a85ab74c
Moriumnasa-Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2021-11-02 22:03:43 +0600

    Add files via upload

---
+++ class6/column.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Day6());
+}
+
+class Day6 extends StatelessWidget {
+  const Day6({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("HELLO Habibi!"),
+        ),
+        body: Container(
+          child: Center(
+              child: Column(
+            children: [
+              Text("Coulmn",
+                  style: TextStyle(fontSize: 50.0, color: Colors.blue)),
+              Text("Coulmn",
+                  style: TextStyle(fontSize: 50.0, color: Colors.green)),
+              Text("Coulmn",
+                  style: TextStyle(
+                      fontSize: 50.0,
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic)),
+              Text("Coulmn",
+                  style: TextStyle(fontSize: 50.0, color: Colors.red)),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ class6/padding.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Day6());
+}
+
+class Day6 extends StatelessWidget {
+  const Day6({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("HELLO Habibi!"),
+        ),
+        body: Container(
+          child: Center(
+              child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      print("BattleShip");
+                    },
+                    child: Text("NOAH Data")),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      print("BattleShip");
+                    },
+                    child: Text("NOAH Data2")),
+              )
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
