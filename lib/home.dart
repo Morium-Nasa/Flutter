84dcf7a5a011109031f2b18086cd916b16ca6e60
Moriumnasa-Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2021-11-13 12:41:16 +0600

    Add files via upload

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:xm/secondpage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Map> myProducts =
+      List.generate(100000, (index) => {"id": index, "name": "Product $index"})
+          .toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('saturday exam'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20),
+            itemCount: myProducts.length,
+            itemBuilder: (BuildContext ctx, index) {
+              return Container(
+                alignment: Alignment.center,
+                child: Text(myProducts[index]["name"]),
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(15)),
+              );
+              // ignore: dead_code
+            }),
+      ),
+    );
+  }
+}
---
+++ lib/secondpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class second extends StatefulWidget {
+  const second({Key? key}) : super(key: key);
+
+  @override
+  _secondState createState() => _secondState();
+}
+
+class _secondState extends State<second> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
