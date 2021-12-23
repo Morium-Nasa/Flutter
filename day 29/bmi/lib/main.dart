20c401f6583c237760110c09eb6ac31901ce46dd
Moriumnasa-Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2021-12-23 12:17:56 +0600

    Add files via upload

---
+++ day 29/bmi/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bmi/screen/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
