b33d19c5ceac84bf11e9ef6069cccbd95cf8d4d0
Moriumnasa-Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2022-01-16 21:19:46 +0600

    Add files via upload

---
+++ Json_file/drawer/lib/jsonlist.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _items = [];
+
+  Future readJson() async {
+    final String response = await rootBundle.loadString('assets/sample.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["items"];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('JSON App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('Load Data'),
+              onPressed: readJson,
+            ),
+
+            // Display the data loaded from sample.json
+            _items.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: _items.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: const EdgeInsets.all(10),
+                          child: ListTile(
+                            leading: Text(_items[index]["id"]),
+                            title: Text(_items[index]["name"]),
+                            subtitle: Text(_items[index]["description"]),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ Json_file/drawer/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:drawer/jsonlist.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
