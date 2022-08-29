8be924be521b5f92af42205e21f42d50eeb7aca4
Mim <76652494+Morium-Nasa@users.noreply.github.com>
2022-08-29 14:00:09 +0600

    Update main.dart

--- CheckboxListTile/day28/lib/main.dart
+++ CheckboxListTile/day28/lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Check box title',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: NewPage(),
     );
